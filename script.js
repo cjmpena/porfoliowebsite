3712f7f84f05d2536b566c951c25158ca9aabc45
Carla Manansala <manansal.carla@gmail.com>
2024-11-21 19:14:52 -0600

    Comment changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/****
+ *
+ * Made By: Carla Manansala
+ * Created: November 21, 2024
+ * Updated:
+ *
+ ***/
 document.querySelector('.btn-talk').addEventListener('click', () => {
     alert("Let's Talk button clicked!");
   });
