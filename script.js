a19808237367ed1faaae3efb506ed1197a230e4b
Carla Manansala <manansal.carla@gmail.com>
2024-11-21 19:19:02 -0600

    Updated comments

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-/****
- *
- * Made By: Carla Manansala
- * Created: November 21, 2024
- * Updated:
- *
- ***/
+// Made By: Carla Manansala
+// Created: November 21, 2024
+// Updated:
+
 document.querySelector('.btn-talk').addEventListener('click', () => {
     alert("Let's Talk button clicked!");
   });
