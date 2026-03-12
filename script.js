35409f08f54df4da00607392aa31eafd1ffd1a99
Carla Manansala <manansal.carla@gmail.com>
2026-03-12 17:50:57 -0500

    Updated photo and phone view

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 // Created: November 21, 2024
 // Updated:
 
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+hamburger.addEventListener('click', () => {
+hamburger.classList.toggle('open');
+  navLinks.classList.toggle('open');
+});
+
+document.querySelectorAll('.nav-close').forEach(link => {
+  link.addEventListener('click', () => {
+  hamburger.classList.remove('open');
+  navLinks.classList.remove('open');
+    });
+});
+
 document.querySelector('.btn-talk').addEventListener('click', () => {
     alert("Let's Talk button clicked!");
   });
