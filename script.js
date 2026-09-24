a20b984a5473a9f24f57a752703ef2de695752c5
Carla Manansala <manansal.carla@gmail.com>
2026-09-23 19:42:10 -0500

    Updated and made text more simplier

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Made By: Carla Manansala
 // Created: November 21, 2024
-// Updated:
+// Updated: September 23, 2026
 
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
@@ -9,6 +9,7 @@ hamburger.classList.toggle('open');
   navLinks.classList.toggle('open');
 });
 
+// Were addign a for each loop to close the nav menu
 document.querySelectorAll('.nav-close').forEach(link => {
   link.addEventListener('click', () => {
   hamburger.classList.remove('open');
@@ -16,6 +17,7 @@ document.querySelectorAll('.nav-close').forEach(link => {
     });
 });
 
+// Button click event listener
 document.querySelector('.btn-talk').addEventListener('click', () => {
     alert("Let's Talk button clicked!");
   });
